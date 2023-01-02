745d9d942238fcf9704e0842cc431aee714c42a2
Srikanthnaid <ksru044gmail.com>
2023-01-02 07:43:02 +0530

    Added

---
+++ JS-Employee/Employee.js
@@ -0,0 +1,12 @@
+/*UC-Check Employee is Present or absent */
+function getAttendance() {
+
+    let checkStatus =  Math.floor(Math.random() * 10) %  2;
+
+    if(checkStatus == 0) {
+        console.log("Employee is Absent");
+    } else {
+        console.log("Employee is Present");
+    }
+}
+    getAttendance();
